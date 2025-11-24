759ee8e2375d86c63f1a670aedd06d8c85dd95b8
ChandanSinghChauhan <chandansinghchauhan2002@gmail.com>
2025-11-24 18:18:18 +0530

    Fix Sky import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,93 +1,86 @@
-import React from 'react'
-import { useState, Suspense, useEffect, useRef } from 'react'
-import { Canvas } from '@react-three/fiber'
+import React, { useState, Suspense, useEffect, useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
 import Loader from '../components/Loader';
-import Island from '../models/Island';
-import Sky from '../models/Sky';
-import Bird from '../models/Bird';
-import Plane from '../models/Plane';
+const Island = React.lazy(() => import('../models/Island'));
+const Sky = React.lazy(() => import('../models/Sky'));
+const Bird = React.lazy(() => import('../models/Bird'));
+const Plane = React.lazy(() => import('../models/Plane'));
 import HomeInfo from '../components/HomeInfo';
 import sakura from '../assets/sakura.mp3';
 import { soundoff, soundon } from '../assets/icons';
-const Home = () => {
 
+const Home = () => {
   const audioRef = useRef(new Audio(sakura));
   audioRef.current.volume = 0.4;
   audioRef.current.loop = true;
+
   const [isRotating, setIsRotating] = useState(false);
   const [currentStage, setCurrentStage] = useState(1);
-
   const [isPlayingMusic, setIsPlayingMusic] = useState(false);
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
   useEffect(() => {
-if(isPlayingMusic){
-  audioRef.current.play();
-}
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-return () => {
-  audioRef.current.pause();
-}
-  }, [isPlayingMusic])
-  const adjustIslandForScreenSize = () => {
-    let screenScale = null;
-    let screenPosition = [0, -6.5, -43];
+  useEffect(() => {
+    if (isPlayingMusic) audioRef.current.play();
+    return () => audioRef.current.pause();
+  }, [isPlayingMusic]);
+
+  const adjustIslandForScreenSize = (width) => {
+    let scale = width < 768 ? [0.8, 0.8, 0.8] : [1, 1, 1];
+    let position = [0, -6.5, -43];
     let rotation = [0.1, 4.7, 0];
+    return [scale, position, rotation];
+  };
 
-    if (window.innerWidth < 768) {
-      screenScale = [0.9, 0.9, 0.9];
-      screenPosition = [0, -6.5, -43];
-    } else {
-      screenScale = [1, 1, 1];
+  const adjustPlaneForScreenSize = (width) => {
+    if (width < 768) {
+      return [[1.5, 1.5, 1.5], [0, -6.5, -43]];
     }
-    return [screenScale, screenPosition, rotation]
-  }
-const adjustPlaneForScreenSize = () => {
-    let screenScale;
-    let screenPosition;
+    return [[3, 3, 3], [0, -4, -4]];
+  };
 
-    if (window.innerWidth < 768) {
-      screenScale = [1.5, 1.5, 1.5];
-      screenPosition = [0, -6.5, -43];
-    } else {
-      screenScale = [3, 3, 3];
-      screenPosition = [0, -4, -4];
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize(windowWidth);
 
-    }
-    return [screenScale, screenPosition]
-  }
+  const [planeScale, planePosition] =
+    adjustPlaneForScreenSize(windowWidth);
 
-  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
-  const [planeScale, planePosition ] = adjustPlaneForScreenSize();
   return (
-    <section className='w-full h-screen relative'>
-      {/* If you need the popup, keep it OUTSIDE the Canvas */}
-      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-       {currentStage && <HomeInfo currentStage= {currentStage} />}
+    <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <HomeInfo currentStage={currentStage} />}
       </div>
-      
-      <Canvas 
-        className={`w-full h-screen transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
+
+      <Canvas
+        dpr={[1, 1.5]} // reduce resolution for mobile
+        performance={{ min: 0.2 }} // performance boost
+        gl={{ antialias: true, preserveDrawingBuffer: false }}
         camera={{ near: 0.1, far: 1000 }}
+        className={`w-full h-screen transparent ${
+          isRotating ? 'cursor-grabbing' : 'cursor-grab'
+        }`}
+        onPointerDown={() => setIsRotating(true)}
+        onPointerUp={() => setIsRotating(false)}
       >
         <Suspense fallback={<Loader />}>
-          {/* Proper Three.js lights */}
-          <directionalLight 
-            position={[1, 1, 1]} 
-            intensity={2} 
-          />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
           <ambientLight intensity={0.5} />
-        
-          <hemisphereLight 
-            skyColor="#b1e1ff" 
-            groundColor="#000000" 
-            intensity={1} 
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
           />
-          
+
           <Bird />
-          < Sky 
-          isRotating={isRotating}
-          />
-          <Island 
+          <Sky isRotating={isRotating} />
+
+          <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
@@ -95,24 +88,26 @@ const adjustPlaneForScreenSize = () => {
             setIsRotating={setIsRotating}
             setCurrentStage={setCurrentStage}
           />
+
           <Plane
-          isRotating = {isRotating}
+            isRotating={isRotating}
             scale={planeScale}
             position={planePosition}
-            rotation = {[0,20,0]}
+            rotation={[0, 20, 0]}
           />
         </Suspense>
       </Canvas>
-      <div className='absolute bottom-2 left-2'> 
+
+      <div className="absolute bottom-2 left-2">
         <img
-        src={!isPlayingMusic ? soundoff : soundon}
-        alt="sound"
-        className='w-10 h-10 cursor-pointer object-contain'
-        onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+          src={!isPlayingMusic ? soundoff : soundon}
+          alt="sound"
+          className="w-10 h-10 cursor-pointer object-contain"
+          onClick={() => setIsPlayingMusic(!isPlayingMusic)}
         />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
