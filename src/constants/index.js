757ed0f2230e631f75213e111c9b2f616440ef36
ChandanSinghChauhan <chandansinghchauhan2002@gmail.com>
2025-12-09 18:37:00 +0530

    updated Socials

--- src/constants/index.js
+++ src/constants/index.js
@@ -151,12 +151,12 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
+        link: 'https://github.com/Chandansingh02082002',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+        link: 'https://www.linkedin.com/in/chandansingh08/',
     }
 ];
 
